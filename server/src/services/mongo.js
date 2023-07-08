07014fcd35aad90463cb4ac1512e61d347dda1d5
suman7802 <mansu7802@gmail.com>
2023-07-08 20:31:32 +0545

    initial

---
+++ server/src/services/mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const path = require("path");
+require("dotenv").config({path: path.resolve(__dirname, ".env")});
+
+// Update below to match your own MongoDB connection string.
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
